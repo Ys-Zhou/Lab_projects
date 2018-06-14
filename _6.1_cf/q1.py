2384ae8b1a18dc76d51526121433ec90529e0083
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-06-14 17:26:13 +0900

    y.zhou

---
+++ _6.1_cf/q1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from DBConnector import GetCursor
+
+df = pd.read_table('user_artists.dat')
+values = df.values.tolist()
+
+with GetCursor() as cur:
+    sql = 'INSERT INTO rating (uid, aid, weight) VALUES (%s, %s, %s)'
+    cur.executemany(sql, values)
