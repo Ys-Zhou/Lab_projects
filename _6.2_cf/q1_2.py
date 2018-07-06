4a9f7c7f4c562328d5e3bc52e9c21c71af98a32c
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-07-06 17:03:33 +0900

    y.zhou

--- _6.2_cf/q1_2.py
+++ _6.2_cf/q1_2.py
@@ -1,9 +1,15 @@
+import time
 from dbconnector import GetCursor
 
 with GetCursor() as cur:
     with GetCursor() as subcur:
         cur.execute('SELECT DISTINCT uid FROM rating')
+
+        start = time.time()
+
         for user in cur:
             subcur.callproc('upred', (user[0],))
             for res in subcur.stored_results():
                 print(res.fetchall())
+
+print(time.time() - start)  # run time = 515.90
