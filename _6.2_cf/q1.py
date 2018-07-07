9f1d76ea78d85d164dfce6859d5bf16c7fa21cf1
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-07-07 21:14:46 +0900

    y.zhou

--- _6.2_cf/q1.py
+++ _6.2_cf/q1.py
@@ -1,42 +1,41 @@
 from dbconnector import GetCursor
 
 with GetCursor() as cur:
-    # Create rating_dict
+    # Step 1: create rating dict
     query = 'SELECT * FROM rating'
     cur.execute(query)
 
     # [[<user>, <item>, <weight>]] -> {<user>: [(<item>, <weight>)]}
     rating_dict = dict()
+
     for row in cur:
         rating_dict.setdefault(row[0], []).append(row[1:3])
 
+    # Step 2: calculate predictions for each user
+    query = ('SELECT usera, userb, sim FROM usersim UNION '
+             'SELECT userb, usera, sim FROM usersim ORDER BY usera')
+    cur.execute(query)
 
-def upred(user_a: int, rec_limit: int) -> list:
-    # known items
-    known_tuple = list(zip(*rating_dict[user_a]))[0]
-
+    current_user = None
+    known_items = None
     # pred_dict:: {<item>: [<rating_sum>, <sim_sum>]}
-    pred_dict = dict()
+    pred_dict = None
 
-    with GetCursor() as cur_:
-        query_ = ('SELECT userb, sim FROM usersim WHERE usera = %d UNION '
-                  'SELECT usera, sim FROM usersim WHERE userb = %d') % (user_a, user_a)
-        cur_.execute(query_)
+    # row:: [<user_a>, <user_b>, <sim>]
+    for row in cur:
+        if row[0] != current_user:
+            if current_user:
+                pred_list = [[k, v[0] / v[1]] for k, v in pred_dict.items()]
+                pred_list.sort(key=lambda x: x[1], reverse=True)
+                print(current_user)
+                print(pred_list[:5])
+            current_user = row[0]
+            pred_dict = dict()
+            known_items = list(zip(*rating_dict[row[0]]))[0]
 
-        # row_:: [<user_b>, <sim>]
-        for row_ in cur_:
-            # pair:: [<item>, <weight>]
-            for pair in rating_dict[row_[0]]:
-                if pair[0] not in known_tuple:
-                    pred_val = pred_dict.setdefault(pair[0], [0, 0])
-                    pred_val[0] += row_[1] * pair[1]
-                    pred_val[1] += row_[1]
-
-    pred_list = [[k, v[0] / v[1]] for k, v in pred_dict.items()]
-    pred_list.sort(key=lambda x: x[1], reverse=True)
-    return pred_list[:rec_limit]
-
-
-for user in rating_dict.keys():
-    print(user)
-    print(upred(user, 5))
+        # pair:: [<item>, <weight>]
+        for pair in rating_dict[row[1]]:
+            if pair[0] not in known_items:
+                pred_val = pred_dict.setdefault(pair[0], [0, 0])
+                pred_val[0] += row[2] * pair[1]
+                pred_val[1] += row[2]
--- _6.2_cf/q2.py
+++ _6.2_cf/q2.py
@@ -1,8 +1,8 @@
 from dbconnector import GetCursor
 
 with GetCursor() as cur:
-    # Create similarity dict
-    query = 'SELECT arta, artb, sim FROM artsim'
+    # Step 1: create similarity dict
+    query = 'SELECT * FROM artsim'
     cur.execute(query)
 
     # [[<item>, <item>, <sim>]] -> {<item_a>: [(<item_b>, <sim>)]}
@@ -12,7 +12,7 @@ with GetCursor() as cur:
         sim_dict.setdefault(row[0], []).append([row[1], row[2]])
         sim_dict.setdefault(row[1], []).append([row[0], row[2]])
 
-    # Calculate prediction for each user
+    # Step 2: calculate predictions for each user
     query = 'SELECT * FROM rating ORDER BY uid'
     cur.execute(query)
 
@@ -25,9 +25,9 @@ with GetCursor() as cur:
     for row in cur:
         if row[0] != current_user:
             if current_user:
-                print(current_user)
                 pred_list = [[k, v[0] / v[1]] for k, v in pred_dict.items() if k not in known_items]
                 pred_list.sort(key=lambda x: x[1], reverse=True)
+                print(current_user)
                 print(pred_list[:5])
             current_user = row[0]
             known_items = []
